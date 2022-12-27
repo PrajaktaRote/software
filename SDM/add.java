7075f6d10c4ceda2a333278b7c9fa52927045053
PrajaktaRote <prajaktarote13@gmail.com>
2022-12-27 22:07:13 +0530

    initial commit

---
+++ SDM/add.java
@@ -0,0 +1,10 @@
+class file{
+    public static void main(String[] args) {
+        int x =10;
+        int y =20;
+	int z = x + y;
+	System.out.println("x : "+x);
+	System.out.println("y : "+y);
+        System.out.println("Addition of two number : "+z);
+    }
+}
